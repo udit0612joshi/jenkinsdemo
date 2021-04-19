70cdfa42f0bbe2ba4ed673be4c13bb71024e39fd
uditjoshi0612 <uditjoshi0612@gmail.com>
2021-04-19 17:11:50 +0530

    Added java code

---
+++ ArrayDemo.java
@@ -0,0 +1,10 @@
+public class ArrayDemo {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        for (int element: array) {
+            System.out.println(element);
+        }
+    }
+}
